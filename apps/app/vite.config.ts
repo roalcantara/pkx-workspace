5c254ea13237aeae0cd22b81f0e32ca8f3ef6fba
Rogério R. Alcântara <rogerio.alcantara@clinicorp.com>
2023-12-22 18:11:30 -0300

    chore(pwa): Enable PWA capability
    Add vite-plugin-pwa which adds PWA, with almost zero config. It adds
    sensible built-in default configuration for common use cases:
    
    - Generate the Web App Manifest: using the manifest plugin option;
    - Configure the Web App Manifest on your application entry point;
    - Generate the service worker: using the strategies plugin option;
    - Generate the script to register the service worker in the browser;
    
    More:
    - https://vite-pwa-org.netlify.app/guide
    - https://vite-pwa-org.netlify.app/assets-generator/cli.html
    - https://github.com/puku0x/nx-react-pwa-sample/commit/3aa5f973daafe0a58b5f0a4ff1019d0d4973c562

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types='vitest' />
+import { VitePWA } from 'vite-plugin-pwa'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -17,7 +18,55 @@ export default defineConfig({
     host: 'localhost'
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon-180x180.png',
+        'maskable-icon-512x512.png'
+      ],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'pkx',
+        description: 'A simple react app',
+        theme_color: '#E05E2E',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 
   // Uncomment this if you are using workers.
   // worker: {
